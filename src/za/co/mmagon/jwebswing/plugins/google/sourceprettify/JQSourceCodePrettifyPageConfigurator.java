07b5fc4b9864ed3234026faffb220ba27d1b938e
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:56 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/google/sourceprettify/JQSourceCodePrettifyPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/google/sourceprettify/JQSourceCodePrettifyPageConfigurator.java
@@ -1,25 +1,18 @@
-/*
- * The MIT License
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * Copyright 2017 GedMarc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.google.sourceprettify;
 
@@ -33,15 +26,22 @@ import za.co.mmagon.jwebswing.plugins.PluginInformation;
  * @since 26 Feb 2017
  *
  */
-@PluginInformation(pluginName = "Google Code Prettify", pluginUniqueName = "google-code-prettify",
-        pluginDescription = "An embeddable script that makes source-code snippets in HTML prettier.",
-        pluginVersion = "1.9.2",
-        pluginDependancyUniqueIDs = "", pluginCategories = "fonts, icons",
-        pluginSubtitle = "Automatically exported from code.google.com/p/google-code-prettify ",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-GoogleSourceCodePrettifyPlugin",
-        pluginSourceUrl = "",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-GoogleSourceCodePrettifyPlugin/wiki",
-        pluginOriginalHomepage = "https://github.com/google/code-prettify")
+@PluginInformation(pluginName = "Google Code Prettify",
+                   pluginUniqueName = "google-code-prettify",
+                   pluginDescription = "An embeddable script that makes source-code snippets in HTML prettier.",
+                   pluginVersion = "1.9.2",
+                   pluginDependancyUniqueIDs = "",
+                   pluginCategories = "code, highlight, source",
+                   pluginSubtitle = "A Javascript module and CSS file that allows syntax highlighting of source code snippets in an html page.",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-GoogleSourceCodePrettifyPlugin",
+                   pluginSourceUrl = "https://github.com/google/code-prettify",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-GoogleSourceCodePrettifyPlugin/wiki",
+                   pluginOriginalHomepage = "https://github.com/google/code-prettify",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/GoogleSourceCodePrettifyPlugin.jar/download",
+                   pluginIconUrl = "bower_components/google-code-prettify/icon.gif",
+                   pluginIconImageUrl = "bower_components/google-code-prettify/google_prettify_example.jpg",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class JQSourceCodePrettifyPageConfigurator extends PageConfigurator
 {
 

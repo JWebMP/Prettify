a8d25a3f6f5c18462d7cd67bf4463ab7eb97b6b8
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:43:40 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,10 @@
+module com.jwebmp.plugins.google.sourceprettify {
+	exports com.jwebmp.plugins.google.sourceprettify;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+	requires java.logging;
+}

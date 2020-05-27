a13d6d1c57ff6ef1b6a02f254eaf53ad25f24a44
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:31:45 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,7 +3,6 @@ module com.jwebmp.plugins.google.sourceprettify {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;

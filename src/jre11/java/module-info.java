27c5db00e7de545bfd28bdd681a26637f6fe7c48
Marc Magon <marc.magon@gmail.com>
2024-02-04 23:41:47 +0200

    Updates for version 2

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,7 +4,7 @@ module com.jwebmp.plugins.google.sourceprettify {
 	exports com.jwebmp.plugins.google.sourceprettify;
 
 	requires com.jwebmp.core;
-	requires com.guicedee.logmaster;
+	
 
 	requires jakarta.validation;
 	requires java.logging;
